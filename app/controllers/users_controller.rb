6ba1b754590d6e491486d1b9b211a6c098c99d9c
Raghu Betina <raghu@firstdraft.com>
2018-01-07 00:10:19 +0000

    2nd draft

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,9 +22,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new
 
-    @user.first_name = params[:first_name]
-    @user.last_name = params[:last_name]
+    @user.username = params[:username]
     @user.password = params[:password]
+    @user.email = params[:email]
 
     save_status = @user.save
 
@@ -51,9 +51,9 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
-    @user.first_name = params[:first_name]
-    @user.last_name = params[:last_name]
+    @user.username = params[:username]
     @user.password = params[:password]
+    @user.email = params[:email]
 
     save_status = @user.save
 
--- db/migrate/20180107000910_create_dishes.rb
+++ db/migrate/20180107000910_create_dishes.rb
@@ -3,7 +3,6 @@ class CreateDishes < ActiveRecord::Migration
     create_table :dishes do |t|
       t.string :name
       t.string :cuisine
-      t.integer :venue_id
 
       t.timestamps
 
