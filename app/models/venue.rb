8a26aae0c3e04433dded66538766dae2d2740c2e
Raghu Betina <raghu@firstdraft.com>
2018-01-09 16:24:26 +0000

    6th draft

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,9 +4,6 @@ class Venue < ApplicationRecord
   has_many   :bookmarks,
              :dependent => :destroy
 
-  has_many   :dishes,
-             :dependent => :destroy
-
   # Indirect associations
 
   # Validations
